55cecadc777f7279e4d28c4b9007faec1a42423b
Vinu Somayaji <1007109+vsomayaji@users.noreply.github.com>
2022-06-08 14:09:52 -0700

    feat: export Language

--- lib/flutter_aira.dart
+++ lib/flutter_aira.dart
@@ -2,6 +2,7 @@ export 'src/models/credentials.dart' show Credentials;
 export 'src/models/feedback.dart' show AgentFeedback, Feedback, Rating;
 export 'src/models/message.dart' show Message;
 export 'src/models/participant.dart' show Participant, ParticipantStatus;
+export 'src/models/profile.dart' show Language;
 export 'src/models/service_request.dart' show ServiceRequest, ServiceRequestState;
 export 'src/models/session.dart' show Session;
 export 'src/models/track.dart' show Track;
