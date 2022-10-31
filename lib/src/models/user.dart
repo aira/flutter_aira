818cb0cdb5b2771f254730b86c8cd6e771c547f4
Israel Painchaud <israel.painchaud@aira.io>
2022-10-31 17:15:57 -0400

    added email and phone number to the User information

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -2,15 +2,17 @@ import 'profile.dart';
 
 class User {
   final int id;
+  final String? email;
   final String firstName;
   final String lastName;
+  final String? phoneNumber;
   final List<Profile> profiles;
-  final bool isGuest;
 
   User.fromJson(Map<String, dynamic> json)
       : id = json['id'],
+        email = json['email'],
         firstName = json['firstName'],
         lastName = json['lastName'] ?? '',
-        isGuest = json['guest'] ?? true, // This information comes from table `user_property_value` and defaults to true in platform (see SubscriptionManager.isGuestSafe(userId))
+        phoneNumber = json['phoneNumber'],
         profiles = (json['accounts'] as List<dynamic>).map((e) => Profile.fromJson(e)).toList(growable: false);
 }
