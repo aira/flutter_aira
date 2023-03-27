ba4fbed14c11be28c605a0ff49fd968bf7a86611
ipainchaud <israel.painchaud@aira.io>
2023-03-27 17:10:11 -0400

    feat: adds fallback procedure when data connection changes or is lost (#73)
    When a user walks outside of a wifi, the application used to lose
    connectivity with the agent... without letting the Explorer know
    anything about the issue. This PR:
    
    lets the user know when a connection is lost (not switching here, but
    simply lost)
    without adding a reconnect button, this PR automatically attempts to
    reconnect as soon as a connection change happens while keeping the user
    aware of that pending reconnection.
    uses a much more reliable reconnection process than a simple "legacy"
    reconnect. (we use a WebRTC restartIce).
    adds a mechanism to lets the user know when a reconnection failed (This
    information wasn't getting back to the Explorer before)... although we
    still have to improve that "legacy" reconnection process which not only
    fails often, but doesn't provide much feedback.

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 import device_info_plus
 import flutter_webrtc
 import package_info_plus
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
