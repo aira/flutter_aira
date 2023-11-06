8b1f5a812dedbd5bea341bc6718d14cda72fbe99
Israel Painchaud <israel.painchaud@aira.io>
2023-11-06 15:46:25 -0500

    made the throttling a little more aggressive to avoid 'PlatformLocalizedException: The endpoint has been modified concurrently'

---
+++ lib/src/throttler.dart
@@ -0,0 +1,18 @@
+class Throttler {
+  Throttler({this.delay = 1000});
+
+  final int delay;
+  DateTime _lastTimestamp = DateTime.fromMillisecondsSinceEpoch(0);
+
+  DateTime get lastTimestamp => _lastTimestamp;
+
+  bool get shouldThrottle {
+    DateTime now = DateTime.now();
+    if (now.difference(_lastTimestamp).inMilliseconds < delay) {
+      // Same throttling delay as `KurrentoRoom.updateLocation`.
+      return true;
+    }
+    _lastTimestamp = now;
+    return false;
+  }
+}
---
+++ test/throttler_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_aira/src/throttler.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Throttler tests', ()
+  {
+    test('First call is never throttled', () async {
+      Throttler throttler = Throttler(delay: 50);
+      await Future.delayed(const Duration(milliseconds: 10));
+      expect(throttler.shouldThrottle, isFalse); // first call is always false
+    });
+
+    test('First call is never throttled even when called long after creation', () async {
+      Throttler throttler = Throttler(delay: 50);
+      await Future.delayed(const Duration(milliseconds: 75));
+      expect(throttler.shouldThrottle, isFalse); // first call is always false
+    });
+
+    test('Should Throttle with expected delay', () async {
+      Throttler throttler = Throttler(delay: 50);
+      expect(throttler.shouldThrottle, isFalse); // first call is always false
+      await Future.delayed(const Duration(milliseconds: 55));
+      expect(throttler.shouldThrottle, isFalse);
+      expect(throttler.shouldThrottle, isTrue); // second call is throttled
+      expect(throttler.shouldThrottle, isTrue);
+      await Future.delayed(const Duration(milliseconds: 30));
+      expect(throttler.shouldThrottle, isTrue);
+      await Future.delayed(const Duration(milliseconds: 21));
+      expect(throttler.shouldThrottle, isFalse);
+      expect(throttler.shouldThrottle, isTrue); // second call is throttled
+    });
+  });
+}
