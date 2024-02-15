b04db5fe51920af7873264668279c25be29690c7
evgeniya.klesto <evgeniya.klesto@aira.io>
2024-02-15 15:29:02 -0800

    add new constructor to the chat.dart model

--- lib/src/models/chat/chat.dart
+++ lib/src/models/chat/chat.dart
@@ -2,12 +2,21 @@ import 'package:flutter_aira/src/models/chat/chat_feedback_info.dart';
 import 'package:flutter_aira/src/models/conversion_extension.dart';
 
 class ChatSessionInfo {
+  ChatSessionInfo.temp()
+      : chatId = 0,
+        userId = 0,
+        createdAt = DateTime.now(),
+        messages = [];
+
   ChatSessionInfo.fromJson(Map<String, dynamic> json)
       : chatId = json['chatId'],
         userId = json['userId'],
         createdAt = (json['createdAt'] as String).dateTimeZ,
-        messages =
-            json['messages'] != null ? (json['messages'] as List).map((e) => ChatMessageInfo.fromJson(e)).toList() : [];
+        messages = json['messages'] != null
+            ? (json['messages'] as List)
+                .map((e) => ChatMessageInfo.fromJson(e))
+                .toList()
+            : [];
 
   final int chatId;
   final int userId;
@@ -25,13 +34,21 @@ enum SenderRole {
 }
 
 class ChatMessageInfo {
+  ChatMessageInfo.temp({required this.role, this.message, this.imageUrl})
+      : id = 0,
+        chatId = 0,
+        senderId = 0,
+        userFeedback = null;
+
   ChatMessageInfo.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         chatId = json['chatId'],
         senderId = json['senderId'],
         role = SenderRole.fromName(json['role']),
         message = json['message'],
-        userFeedback = json['userFeedback'] != null ? ChatFeedbackInfo.fromJson(json['userFeedback']) : null,
+        userFeedback = json['userFeedback'] != null
+            ? ChatFeedbackInfo.fromJson(json['userFeedback'])
+            : null,
         imageUrl = json['imageUrl'];
 
   final int id;
