b3fcbec9b5ec3081534e4b2f67e4f70f2df3e1c7
evgeniya.klesto <evgeniya.klesto@aira.io>
2024-02-21 11:07:05 -0800

    Change ChatFeedbackInfo

--- lib/src/models/chat/chat_feedback_info.dart
+++ lib/src/models/chat/chat_feedback_info.dart
@@ -1,9 +1,10 @@
 class ChatFeedbackInfo {
-  final String authorId;
+  final int authorId;
   final int rating;
   final String? comment;
 
-  ChatFeedbackInfo({required this.authorId, required this.rating, this.comment});
+  ChatFeedbackInfo(
+      {required this.authorId, required this.rating, this.comment});
 
   ChatFeedbackInfo.fromJson(Map<String, dynamic> json)
       : authorId = json['authorId'],
