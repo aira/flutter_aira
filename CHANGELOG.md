c4d99fa084c103c4f5ad145e08d6765dd73d72e0
smunozp <61166720+smunozp@users.noreply.github.com>
2024-03-08 17:09:33 -0500

    Left event support and change default in ServiceRequestState (#119)
    Handle Left event triggered when there is a call transfer and Agent 1
    leaves the call

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.2.0] - 2024-03-08
+
+### Changed
+- ### Added Handling Case "LEFT" on `_updateServiceRequestStatus` on transfer calls this, without this version if a transfer occurs in the call it will terminate the call for the explorer (bug) (#119)
+
 ## [2.1.2] - 2024-03-06
 ### Changed
 - Updated mqtt's websocket protocol to align with newer version of RabbitMQ. This change is backward compatible. (#118)
--- lib/src/room.dart
+++ lib/src/room.dart
@@ -645,13 +645,22 @@ class KurentoRoom extends ChangeNotifier implements Room {
           await _updateParticipantStatus();
         }
         break;
+      // Handles the case when the Agent leaves the room on a call transfer, name is turned to null to represent that agent is no longer in the call
+      case 'LEFT':
+        _agentName = null;
+        notifyListeners();
+        break;
 
-      // All of the other status values -- END, CANCEL, etc. -- are end states.
-      default:
+      case 'END':
+      case 'CANCEL':
         if (_serviceRequestState != ServiceRequestState.ended) {
           _serviceRequestState = ServiceRequestState.ended;
           if (!_isDisposed) notifyListeners();
         }
+        break;
+
+      default:
+        _log.warning('ignoring unsupported service request status=$status');
     }
   }
 
