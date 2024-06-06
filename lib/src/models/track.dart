b98d7ec782b8ed515d1931f75dd8ca2ba963f6ef
Hosein Haqiqian <hosein1398@gmail.com>
2024-06-06 13:38:40 -0400

    chore: create track kind enum

--- lib/src/models/track.dart
+++ lib/src/models/track.dart
@@ -2,11 +2,15 @@ class Track {
   int id;
   int participantId;
   int? incomingTrackId;
+  TrackKind? kind;
 
   Track.fromJson(Map<String, dynamic> json)
       : id = int.parse(json['id']),
         participantId = json['participantId'],
-        incomingTrackId = int.tryParse(json['incomingTrackId'] ?? '');
+        incomingTrackId = int.tryParse(json['incomingTrackId'] ?? ''),
+        kind = json['audio'] ?? false ? TrackKind.audio : (json['video'] ?? false ? TrackKind.video : null);
 
   bool get isOutgoing => incomingTrackId == null;
 }
+
+enum TrackKind { audio, video }
