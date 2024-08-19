a95c7f2da2944031c8dc14db436250a3c644d6a5
evgeniya.klesto <evgeniya.klesto@aira.io>
2024-08-19 16:39:45 -0700

    Add AccessAi field to User Profile

--- lib/src/models/account.dart
+++ lib/src/models/account.dart
@@ -15,13 +15,35 @@ enum AccountType {
   }
 }
 
+enum AccessAi {
+  fullAccess('FULL_ACCESS'),
+  noHistory('NO_HISTORY'),
+  noAccess('NO_ACCESS');
+
+  final String value;
+
+  const AccessAi(this.value);
+
+  /// Gets the [AccessAi] from the given value.
+  ///
+  /// If the value is not found, [noAccess] is returned.
+  static AccessAi fromValue(String? value) {
+    return AccessAi.values
+        .firstWhere((e) => e.value == value, orElse: () => AccessAi.noAccess);
+  }
+}
+
 class Account {
   final int id;
   final String name;
   final AccountType type;
 
+  ///The Account(Profile) access limitations to AccessAI.
+  final AccessAi? accessAI;
+
   Account.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         name = json['name'],
-        type = AccountType.fromName(json['accountType']);
+        type = AccountType.fromName(json['accountType']),
+        accessAI = AccessAi.fromValue(json['accessAi']);
 }
